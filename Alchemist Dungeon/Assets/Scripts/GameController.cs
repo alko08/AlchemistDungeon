661fb1a1629db34ac52ac53cec7634bfa5fe1978
Alexander Koppel <alexk@thekoppels.net>
2022-02-23 14:51:26 -0500

    Implemented HUD & Win Scene
    This also meant I had to fix all the menu assets and game Controller to get it all to work together.

--- Alchemist Dungeon/Assets/Scripts/GameController.cs
+++ Alchemist Dungeon/Assets/Scripts/GameController.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameHandler : MonoBehaviour
+public class GameController : MonoBehaviour
 {
     public static string SceneDied = "MainMenu";
+    private int score;
+    public GameObject scoreTextObject;
+    private bool gameOver;
 
     // Start is called before the first frame update
     void Start (){
         string thisLevel = SceneManager.GetActiveScene().name;
+        gameOver = false;
+        score = 0;
         if ((thisLevel != "SceneLose") && (thisLevel != "SceneWin")){
             SceneDied = thisLevel;
         }
     }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape")) {
+            QuitGame();
+        }
+    }
+
     public void StartGame() {
         SceneManager.LoadScene("Level1");
     }
@@ -35,9 +49,17 @@ public class GameHandler : MonoBehaviour
         SceneManager.LoadScene(SceneDied);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AddScore (int newScoreValue) {
+        score += newScoreValue;
+        UpdateScore ();
+        if (score >= 20 && !gameOver) {
+            gameOver = true;
+            SceneManager.LoadScene("SceneWin");
+        }
+    }
+
+    void UpdateScore () {
+        Text scoreTextB = scoreTextObject.GetComponent<Text>();
+        scoreTextB.text = "" + score;
     }
 }
